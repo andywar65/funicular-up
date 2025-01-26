971de582704106955748f09e2ec1b38e872da7f3
andywar65 <andy.war1965@gmail.com>
2025-01-26 16:45:37 +0100

    added folder detail view

--- src/funicular_up/urls.py
+++ src/funicular_up/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import FolderListView
+from .views import FolderDetailView, FolderListView
 
 app_name = "funicular_up"
 urlpatterns = [
     path("folder/", FolderListView.as_view(), name="folder_list"),
+    path("folder/<pk>/", FolderDetailView.as_view(), name="folder_detail"),
 ]
--- src/funicular_up/views.py
+++ src/funicular_up/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Folder
 
@@ -12,3 +12,8 @@ class FolderListView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context["object_list"] = context["object_list"].with_tree_fields()
         return context
+
+
+class FolderDetailView(LoginRequiredMixin, DetailView):
+    model = Folder
+    template_name = "funicular_up/folder_detail.html"
