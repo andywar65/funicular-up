9cf0659e06ecf01285a4d421d2dcf402b35c8573
andywar65 <andy.war1965@gmail.com>
2025-01-26 17:03:34 +0100

    added funicular manager group

--- src/funicular_up/apps.py
+++ src/funicular_up/apps.py
@@ -1,6 +1,24 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.utils.translation import gettext as _
+
+
+def create_funicular_up_group(sender, **kwargs):
+    from django.contrib.auth.models import Group, Permission
+    from django.contrib.contenttypes.models import ContentType
+
+    grp, created = Group.objects.get_or_create(name=_("Funicular Manager"))
+    if created:
+        types = ContentType.objects.filter(app_label="funicular_up").values_list(
+            "id", flat=True
+        )
+        permissions = Permission.objects.filter(content_type_id__in=types)
+        grp.permissions.set(permissions)
 
 
 class FunicularUpConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "funicular_up"
+
+    def ready(self):
+        post_migrate.connect(create_funicular_up_group, sender=self)
