cc595f57f3fc23aa1cc5e4e39298c3983abd152d
andywar65 <andy.war1965@gmail.com>
2025-02-01 16:58:04 +0100

    adding django cors headers

--- project/project/settings.py
+++ project/project/settings.py
@@ -32,6 +32,11 @@ DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "apperilli.opalstacked.com"]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
 
 # Application definition
 
@@ -43,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third party
+    "corsheaders",
     "easy_thumbnails",
     "filer",
     "leaflet",
@@ -53,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
