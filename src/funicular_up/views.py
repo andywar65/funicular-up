19214b84ece5e36b80409a5e807a923c0f5719c4
andywar65 <andy.war1965@gmail.com>
2025-02-20 09:49:52 +0100

    adding form for multiple file uploads

--- src/funicular_up/views.py
+++ src/funicular_up/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.forms import ModelForm
@@ -57,6 +58,28 @@ class FolderInitialCreateView(FolderCreateView):
         initial = super().get_initial()
         initial["parent"] = get_object_or_404(Folder, id=self.kwargs["pk"])
         return initial
+
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
+
+class FileFieldForm(forms.Form):
+    file_field = MultipleFileField()
 
 
 class FolderDetailView(LoginRequiredMixin, DetailView):
