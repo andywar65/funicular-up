a11258042b8aef45c8d94c0498a3d5eddc43cb43
andywar65 <andy.war1965@gmail.com>
2025-03-05 21:36:38 +0100

    add map script js

---
+++ src/funicular_up/static/funicular_up/js/map_script.js
@@ -0,0 +1,27 @@
+function map_init(map, options) {
+
+    function onEachFeature(feature, layer) {
+      if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent.content, {minWidth: 256});
+      }
+    }
+
+    const base_map = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 19,
+      }).addTo(map);
+
+    const marker_layer = L.layerGroup().addTo(map);
+
+    function getCollections() {
+      // add objects to layers
+      collection = JSON.parse(document.getElementById("marker_data").textContent);
+      for (marker of collection.features) {
+        L.geoJson(marker, {onEachFeature: onEachFeature}).addTo(marker_layer);
+      }
+      // fit bounds
+      map.fitBounds(L.geoJson(collection).getBounds(), {padding: [30,30]});
+    }
+
+    getCollections()
+}
